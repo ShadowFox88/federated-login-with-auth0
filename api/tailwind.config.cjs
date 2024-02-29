5dff9588a00fcbf46affd1ce9b4441615cc1171e
cyrus01337 <cyrus21337@gmail.com>
2024-02-29 17:52:48 +0000

    Setup as an API

--- api/tailwind.config.cjs
+++
@@ -1,7 +0,0 @@
-const tailwindCSSTypography = require("@tailwindcss/typography");
-
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["src/app/**/*.tsx"],
-    plugins: [tailwindCSSTypography],
-};
